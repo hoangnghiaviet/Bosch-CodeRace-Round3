118964b0d19d2c6df4cd6037252fb73252567cbd
Hoang Nghia Viet <viethoangnghia@gmail.com>
2023-06-07 11:29:29 +0700

    update

--- task3.py
+++ task3.py
@@ -1,7 +1,6 @@
 import os
 from github import Github
-from github.GithubException import UnknownObjectException
-from github.GithubException import GithubException
+from github.GithubException import UnknownObjectException, GithubException
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="secret.env")
@@ -18,20 +17,24 @@ repository = g.get_repo(repository_name)
 
 # Get the branch
 branch_name = os.getenv("BRANCH_NAME")
-branch_name = os.getenv("BRANCH_NAME")
 try:
     branch = repository.get_branch(branch_name)
     print("Branch already exists:", branch_name)
 except GithubException:
-    # Create a new branch
-    try:
-        empty_file_path = 'README.md'
-        empty_file_content = 'This is the intial commit to create a new branch'
-        repository.create_file(empty_file_path, 'Initial empty file', empty_file_content, branch=branch_name)
-        branch = repository.get_branch(branch_name)
-        print("Created new branch:", branch_name)
-    except UnknownObjectException as e:
-        print("Error creating branch:", e)
+    # Check if any branch exists
+    branches = repository.get_branches()
+    if branches.totalCount > 0:
+        # Use the first existing branch as a base for the new branch
+        base_branch = branches[0]
+    else:
+        # Create a dummy file to initialize the repository if it's empty
+        repository.create_file("README.md", "Initial commit", "This is the initial commit.", branch=branch_name)
+        # Use the created branch as a base for the new branch
+        base_branch = repository.get_branch(branch_name)
+
+    # Create a new branch based on the chosen base branch
+    ref = repository.create_git_ref(ref=f"refs/heads/{branch_name}", sha=base_branch.commit.sha)
+    branch = repository.get_branch(branch_name)
 
 # Overwrite the file if it exists
 file_path = 'ECU_Requirement.rst'
